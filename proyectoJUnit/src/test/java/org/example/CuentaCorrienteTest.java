09dd4d3b343e2b733f42eacf76b33c8a78f0744c
gil.egain20 <ainaragilegeadam@gmail.com>
2020-10-14 10:03:02 +0200

    otro proyecto de prueba

---
+++ proyectoJUnit/src/test/java/org/example/CuentaCorrienteTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CuentaCorrienteTest {
+
+    @Test
+    @DisplayName("Ingreso")
+    public void depositarDinero(){
+
+
+
+    }
+}
